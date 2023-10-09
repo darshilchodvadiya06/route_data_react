8296f7ad24fba5586646548fa92cc05e7af995d2
Darshil-Chodvadiya <darshilchodvadiya59@gmail.com>
2023-10-09 08:50:11 +0530

    Route data with fetch Fake API

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import { Routes, Route } from 'react-router-dom'
+import Navbar from './Navbar';
+import Home from './Components/Home';
+import Product from './Components/Product';
+import Detail from './Components/Detail';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Navbar />
+      <Routes>
+        <Route path='/' exact element={<Home />} />
+        <Route path='/product' exact element={<Product />} />
+        <Route path='/:id' exact element={<Detail />} />
+      </Routes>
+    </>
   );
 }
 
